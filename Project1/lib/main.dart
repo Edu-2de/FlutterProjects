65e80492a5cb198744d54d86b336dffa88d96800
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2025-09-16 15:12:29 -0300

    fix: Update app title and formatting in main.dart

--- Project1/lib/main.dart
+++ Project1/lib/main.dart
@@ -10,9 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple
+        ),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
